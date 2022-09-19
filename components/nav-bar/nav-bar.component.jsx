b71a4eae73fc6db729d454467d098d956c839354
Shahar Har Nesher <Shaharhn@OutLook.Com>
2022-09-19 11:22:01 +0300

    before fb

---
+++ components/nav-bar/nav-bar.component.jsx
@@ -0,0 +1,9 @@
+import "./nav-bar.styles.css"
+const NavBar = (props) => {
+    return (
+        <div className="nav-bar">
+            <h1>this is a nav bar</h1>
+        </div>
+    )
+}
+export default NavBar;
