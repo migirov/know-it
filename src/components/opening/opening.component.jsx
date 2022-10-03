c347f6eaae5ee846110ae6aa79860b21d83838d0
Shahar Har Nesher <Shaharhn@OutLook.Com>
2022-10-03 21:33:15 +0300

    text fix

--- src/components/opening/opening.component.jsx
+++ src/components/opening/opening.component.jsx
@@ -12,7 +12,7 @@ const Opening = (props) => {
     return (
         <div className="opening">
             <h1 className="main-header">כנס חדשנות בלמידה</h1>
-            <div className="opening-txt">כנס חדשני ויחיד מסוגו בנושא חדשנות בלמידה. הכנס מיועד למנהלי הדרכה בכירים - מפקדים בהכשרות (רס״ן ומעלה) ולאנשי פיתוח הדרכה בצה״ל ומחוצלו.</div>
+            <div className="opening-txt">כנס חדשני ויחיד מסוגו בנושא חדשנות בלמידה. הכנס מיועד למנהלי הדרכה בכירים - מפקדים בהכשרות (רס״ן ומעלה) ולאנשי פיתוח הדרכה בצה״ל ומחוצה לו.</div>
             <div className="opening-txt2">הכנס יתקיים בתאריך 22.11.2022 <br></br>בקריית ההדרכה.</div>
             <button className="register-btn" onClick={handleClick}>לפרטים נוספים</button>
             <AddToCalendar/>
